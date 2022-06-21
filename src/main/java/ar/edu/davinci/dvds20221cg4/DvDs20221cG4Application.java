d255e8dce77d35e971edc7af3f438f0b11759df0
Alberto Bossio <alberto.federico.bossio@gmail.com>
2022-06-21 20:11:05 -0300

    Agregado del proyecto Java con SpringBoot

---
+++ src/main/java/ar/edu/davinci/dvds20221cg4/DvDs20221cG4Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20221cg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20221cG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20221cG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20221cg4/DvDs20221cG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20221cg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20221cG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
